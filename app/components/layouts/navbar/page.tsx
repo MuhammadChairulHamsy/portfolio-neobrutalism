0b3679fd3bcfff8eff60354ff7b16bc22e5a0c3a
HamsyDev <mach.hamsy@gmail.com>
2026-04-21 11:10:10 +0700

    feat: create Navbar component

---
+++ app/components/layouts/navbar/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+import { Tooltip } from "../../retroui/Tooltip";
+
+const Navbar = () => {
+  return (
+    <nav className="hidden md:flex justify-between items-center mb-20 p-4 bg-white sticky top-4 z-50 border-2 border-black rounded-xl shadow-[4px_4px_0px_0px_#000] transition-all duration-200 hover:shadow-[1px_1px_0px_0px_#000] hover:translate-x-1 hover:translate-y-1">
+      <h1 className=" text-xl md:text-2xl font-black bg-yellow-300 border-2 border-black px-3 py-1 -rotate-2 cursor-pointer shadow-[4px_4px_0px_0px_#000] transition-all duration-100 active:translate-x-1 active:translate-y-1 active:shadow-none active:scale-95">
+        HAMSY
+      </h1>
+
+      {/* Menu */}
+      <div className="flex gap-6 font-bold text-sm">
+        <Link
+          href="/"
+          className="px-2 py-1 transition-all hover:bg-yellow-300 hover:text-black hover:border-2 hover:border-black hover:shadow-[2px_2px_0px_0px_#000]"
+        >
+          Home
+        </Link>
+        <Link
+          href="/about"
+          className="px-2 py-1 transition-all hover:bg-cyan-300 hover:text-black hover:border-2 hover:border-black hover:shadow-[2px_2px_0px_0px_#000]"
+        >
+          About Me
+        </Link>
+        <Link
+          href="/project"
+          className="px-2 py-1 transition-all hover:bg-pink-300 hover:text-black hover:border-2 hover:border-black hover:shadow-[2px_2px_0px_0px_#000]"
+        >
+          Project
+        </Link>
+        <Link
+          href="/contact"
+          className="px-2 py-1 transition-all hover:bg-orange-300 hover:text-black hover:border-2 hover:border-black hover:shadow-[2px_2px_0px_0px_#000]"
+        >
+          Contact
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Archivo_Black, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const archivoBlack = Archivo_Black({
   subsets: ["latin"],
+  weight: "400",
+  variable: "--font-head",
+  display: "swap",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const space = Space_Grotesk({
   subsets: ["latin"],
+  weight: "400",
+  variable: "--font-sans",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,11 +27,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body className={`${archivoBlack.variable} ${space.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
